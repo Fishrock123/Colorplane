6e930e85df4a39da8e3e88eba50bbd112051ff0d
Fishrock123 <fishrock123@rocketmail.com>
2014-01-16 13:09:19 -0500

    Ported from jQuery.
    Now using vanilla javascript.

--- js/colorplane.js
+++ js/colorplane.js
@@ -10,59 +10,57 @@
 //
 // *********************************
 
-(function($){
-  var ColorPlane = function(element, options) {
-    this.$el           = $(element || '<div />').addClass('colorplane')
-    this.$instructions = $('<div class="colorplane-selected-color"><div class="colorplane-instructions">Click to select a color</div></div>')
-    this.$canvas       = $('<canvas />')
-    this.$color        = this.$canvas.css("background")
-    this.context       = this.$canvas[0].getContext('2d')
-    this.options       = options || {}
+(function(container){
+  var ColorPlane = function(element) {
+    this.$el           = (element || document.getElementById('colorplane'))
+    this.$instructions = '<div id="colorplane-selected-color"><div id="colorplane-instructions">Click to select a color</div></div>'
+    this.$canvas       = document.createElement('canvas')
+    this.$color        = this.$canvas.style.background
+    this.context       = this.$canvas.getContext('2d')
+    this.hexColor      = '#000'
+
+    this.$el.classList.add('colorplane')
 
     this.render()
   }
 
-  ColorPlane.fn = ColorPlane.prototype
-
-  ColorPlane.fn.render = function(){
-    this.$el.html(this.$canvas)
-    this.$el.prepend(this.$instructions)
+  ColorPlane.prototype.render = function() {
+    this.$el.appendChild(this.$canvas)
+    this.$el.insertAdjacentHTML('beforebegin', this.$instructions)
     this.context.fillStyle = this.$color
     this.addListeners()
   }
 
-  ColorPlane.fn.addListeners = function() {
-    this.$canvas.bind('touchmove', function(e) {
+  ColorPlane.prototype.addListeners = function() {
+    var that = this
+    this.$canvas.addEventListener('touchmove', function(e) {
       e.preventDefault()
-      var touch    = e.originalEvent.changedTouches[0]
-        , hexColor = this.getHexColor(touch)
-      this.showColor(hexColor)
+      that.getHexColor(e.originalEvent.changedTouches[0])
+      that.showColor()
+    })
+    this.$canvas.addEventListener('touchend', function() {
+      that.saveCurrentColor()
+    })
 
-      this.$canvas.one('touchend', function() {
-        this.saveCurrentColor(hexColor)
-      }.bind(this))
-    }.bind(this))
+    this.$canvas.addEventListener('mousemove', function(e) {
+      that.getHexColor(e)
+      that.showColor()
+    })
 
-    this.$canvas.bind('mousemove', function(e) {
-      var hexColor = this.getHexColor(e)
-      this.showColor(hexColor)
-
-      this.$canvas.one('click', function() {
-        this.saveCurrentColor(hexColor)
-      }.bind(this))
-    }.bind(this))
+    this.$canvas.addEventListener('click', function() {
+      that.saveCurrentColor()
+    })
   }
 
-  ColorPlane.fn.getHexColor = function(e) {
-    var canvasOffset = this.$canvas.offset()
-      , X = e.pageX - canvasOffset.left
-      , Y = e.pageY - canvasOffset.top
+  ColorPlane.prototype.getHexColor = function(e) {
+    var X = e.pageX - this.$canvas.offsetLeft
+      , Y = e.pageY - this.$canvas.offsetTop
       , canvasX = Math.floor(X/2)
       , canvasY = Math.floor(Y)
       , canvasZ = parseInt(canvasX.toString().slice(0,2))
                 + parseInt(canvasY.toString().slice(0,2))
 
-    return "#" + normalizeHex(canvasX) + normalizeHex(canvasY) + normalizeHex(canvasZ)
+    return this.hexColor = "#" + normalizeHex(canvasX) + normalizeHex(canvasY) + normalizeHex(canvasZ)
   }
 
   function normalizeHex(value) {
@@ -93,30 +91,31 @@
       return value
   }
 
-  ColorPlane.fn.showColor = function(hexColor) {
-    this.context.fillStyle = hexColor
-    this.context.fillRect(0,0, this.$canvas.width(), this.$canvas.height())
+  ColorPlane.prototype.showColor = function() {
+    this.context.fillStyle = this.hexColor
+    this.context.fillRect(0,0, this.$canvas.scrollWidth, this.$canvas.scrollHeight)
 
-    $('#colorplane-current-hex').html(hexColor)
-    $('#colorplane-current-hex').css("color", hexColor)
+    var elem = document.getElementById('colorplane-current-hex')
+    elem.textContent = this.hexColor
+    elem.style.color = this.hexColor
   }
 
-  ColorPlane.fn.saveCurrentColor = function(hexColor) {
-    this.$el.trigger('change', hexColor)
+  ColorPlane.prototype.saveCurrentColor = function() {
+    var evt = document.createEvent('HTMLEvents')
+    evt.initEvent('change', false, true)
+    this.$el.dispatchEvent(evt)
 
-    $('#colorplane-selected-hex').html(hexColor)
-    $('#colorplane-selected-hex').css("color", hexColor)
-    $('.colorplane-selected-color').css("background", hexColor)
-    $('.colorplane-instructions').html("Color selected!")
+    var elem = document.getElementById('colorplane-selected-hex')
+    elem.textContent = this.hexColor
+    elem.style.color = this.hexColor
+
+    document.getElementById('colorplane-selected-color').style.background = this.hexColor
+    document.getElementById('colorplane-instructions').textContent = "Color selected!"
   }
 
-  $.fn.colorplane = function(options){
-    return $(this).each(function(){
-      $(this).data('colorplane', new ColorPlane(this, options))
-    })
-  }
-})(jQuery)
+  container.ColorPlane = ColorPlane
+})(window)
 
-jQuery(function($){
-  $('.colorplane').colorplane()
+document.addEventListener('DOMContentLoaded', function() {
+  new ColorPlane()
 })
